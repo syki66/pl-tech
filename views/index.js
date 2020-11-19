c82d5f0859272d2902a3902643f5d9fc22e1641c
syki66 <syeonki66@gmail.com>
2020-11-20 00:04:08 +0900

    add autoSlide func & organize structure

---
+++ views/index.js
@@ -0,0 +1,3 @@
+// 외부 html 파일 div 내부로 임포트하기
+
+document.querySelectorAll('.carousel__item')[0].innerHTML='<object type="text/html" data="src/pages/1.html" ></object>';
--- views/src/carousel.js
+++ views/src/carousel.js
@@ -13,21 +13,24 @@ slides.forEach((e, i) => {
     slidePointer.push(e.style.left)
 })
 
-// 버튼 리스너
-let current = 0;
-nextBtn.addEventListener('click', ()=>{
+// 다음 슬라이드로 넘기기
+function slideNext () {
     (current >= slidePointer.length-1) ? (current = 0) : (current += 1);
     container.style.transform = `translateX(-${slidePointer[current]})`;
-    // console.log(current);
-});
+}
 
-prevBtn.addEventListener('click', ()=>{
+// 이전 슬라이드로 넘기기
+function slidePrev() {
     (current <= 0) ? (current = slidePointer.length - 1) : (current -= 1);
     container.style.transform = `translateX(-${slidePointer[current]})`;
-    // console.log(current);
-});
+}
 
-// 네비게이션
+// 버튼 리스너
+let current = 0;
+nextBtn.addEventListener('click', () => slideNext());
+prevBtn.addEventListener('click', () => slidePrev());
+
+// 네비게이션 html 요소 추가
 slidePointer.map((e, i) => {
     var indicator = document.createElement("div");
     indicator.classList.add("carousel__indicator");
@@ -37,12 +40,13 @@ slidePointer.map((e, i) => {
     navigation.appendChild(indicator);
 });
 
+// 네이게이션 위치 조절
 const navWidth = navigation.getBoundingClientRect().width;
 const indicatorWidth = document.querySelector(".carousel__indicator").getBoundingClientRect().width;
 navigation.style.transform = `translateX(-${navWidth/2}px)`;
 
+// 네비게이션에서 원하는곳 골라서 클릭할수 있도록함
 const indicatorArray = document.querySelectorAll('.carousel__indicator');
-
 indicatorArray.forEach((each, i)=>{
     each.addEventListener('click', ()=>{
         container.style.transform = `translateX(-${slidePointer[i]})`;
@@ -50,6 +54,7 @@ indicatorArray.forEach((each, i)=>{
     });
 });
 
+// 네비게이션에 현재 슬라이드 위치 표시
 carousel.addEventListener('click', ()=>{
     indicatorArray.forEach((e,i)=>{
         indicatorArray[i].classList.remove('current__carousel');
@@ -57,9 +62,8 @@ carousel.addEventListener('click', ()=>{
     indicatorArray[current].classList.add('current__carousel');
 });
 
-
-
-
+// 자동 슬라이드 기능
+setInterval(() => nextBtn.click(), 5000);
 
         // 동적으로 크기 바뀌게 하려면 이걸로
         // function getSliderSize (){
