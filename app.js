06d17b1208c810578d4c6c507c2a7bf631a612b2
saltcoffee <ybell1028@gmail.com>
2020-11-19 18:43:37 +0900

    start watchFile

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
+const fs = require('fs');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-//static 미들웨어는 특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 만들어줍니다. 
-const static = require('serve-static');
 const util = require('./middleware/util');
+const model = require('./models/figures');
+const inputController = require('./routes/input/input.controller');
 
 const app = express();
 
@@ -42,10 +43,43 @@ let server = http.createServer(app);
 
 //single thread server
 server.listen(app.get('port'), ip, function () {
-    // console.log('ip : ' + ip + ' | port : ' + app.get('port'));
+    console.log('ip : ' + ip + ' | port : ' + app.get('port'));
     console.log('server is running');
 });
 
 server.on('connection', function (socket) {
     console.log('클라이언트가 접속했습니다.');
+});
+
+const figurePath = 'figures.txt'
+
+fs.watchFile(figurePath, (curr, prev) => {
+    const body = { filepath: figurePath }; // post 요청 시 보내는 body 라고 가정
+    const header = {
+        "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'ko- KR, ko; q = 0.9, en - US; q = 0.8, en; q = 0.7',
+        'Cache-Control': 'max-age=0',
+        "Connection": 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Referer': 'http://https://localhost/input/2',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+      }
+    let link = 'https://localhost/input/2'
+    axios({
+        address: '192.168.0.2',
+        url: link,
+        port: 3000,
+        headers : header,
+        method: "POST",
+        data: body
+    })
+    .then(response => {
+       console.log(response);
+       console.log('watchFile success');
+    })
+    .catch(err => {
+        console.log(err);
+        console.log('watchFile error');
+    });
 });
