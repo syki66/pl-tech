a9f580ba2e0ff8c2c4ec1f00613ac6c70018d544
syki66 <syeonki66@gmail.com>
2020-11-20 00:59:31 +0900

    add test data to 1.html

--- views/index.js
+++ views/index.js
@@ -1,3 +1,6 @@
 // 외부 html 파일 div 내부로 임포트하기
 
-document.querySelectorAll('.carousel__item')[0].innerHTML='<object type="text/html" data="src/pages/1.html" ></object>';
+
+document.querySelectorAll('.carousel__item').forEach((e,i)=>{
+    e.innerHTML=`<object type="text/html" data="src/pages/${i+1}.html" ></object>`;
+});
