26ef3ba7a6f6d1d70c9e5288d4ed1af7755b0c6a
saltcoffee <ybell1028@gmail.com>
2020-11-19 21:09:56 +0900

    File modification detect function

--- routes/rotate/index.js
+++ routes/rotate/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./rotate.controller');
 
+router.get('/', controller.responseFigures);
+
 module.exports = router;
--- routes/rotate/rotate.controller.js
+++ routes/rotate/rotate.controller.js
@@ -1,5 +1,18 @@
+const e = require('express');
 const util = require('../../middleware/util');
+const inputController = require('../input/input.controller');
 
-exports.readDCS = (req, res) => {
-    console.log('called readDCS');
-}
+exports.responseFigures = (req, res) => {
+    let data = inputController.parsed;
+    if(data === null || data === undefined) {
+        const err = new Error('parsed data is not exist!');
+        console.dir(err);
+        console.log('파싱된 데이터가 존재하지 않습니다.\n');
+        res.status(500);
+        res.json(util.successFalse(err, '파싱된 데이터가 존재하지 않습니다.'));
+    } else {
+        console.log(data);
+        res.status(201);
+        res.json(util.successTrue(data));
+    }
+};
