a940d284fcdb426558866ef5dcb37b3d2e819dc2
syki66 <syeonki66@gmail.com>
2020-11-21 21:30:07 +0900

    add clock func & put json in localstorage

---
+++ views/src/fetchJson.js
@@ -0,0 +1,15 @@
+const url = "http://localhost:3000/rotate";
+
+
+fetch(url)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (json) {
+        if (!json.success) {
+            alert('서버에서 데이터를 가져오는데 실패하였습니다.');
+        } else{
+            localStorage.setItem("json", JSON.stringify(json))
+            // 커라젤 숨기고 있다가 여기서 로딩 끝나면 펼쳐주기
+        }
+    })
