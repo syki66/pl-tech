7f6cc9b77dd53b6dd04aea27f54f87424bbf252e
tomato8160 <tpgus2303@naver.com>
2020-11-25 00:14:57 +0900

    connected with mysql

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const router = express.Router();
 
 const input = require('./input');
 const rotate = require('./rotate');
-const writenotice = require('./writenotice');
 const notice = require('./notice');
 
 router.use('/input', input);
 router.use('/rotate', rotate);
-router.use('/writenotice', writenotice)
 router.use('/notice', notice);
 
-
 module.exports = router;
+
