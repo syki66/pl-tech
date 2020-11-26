51840561a7141fc295f679d8a803309bd68bf1aa
syki66 <syeonki66@gmail.com>
2020-11-26 21:17:39 +0900

    edit page1 & add page2, page3

--- views/src/clock.js
+++ views/src/clock.js
@@ -1,4 +1,6 @@
-const clock = document.querySelector(".js-clock");
+const clock_1 = document.querySelector(".js-clock__1");
+const clock_2 = document.querySelector(".js-clock__2");
+const clock_3 = document.querySelector(".js-clock__3");
 
 const YY = document.querySelector('.year');
 const MM = document.querySelector('.month');
@@ -16,7 +18,12 @@ function getTime() {
     const hours = date.getHours();
     const seconds = date.getSeconds();
     
-    clock.innerText = `${year}-${month}-${today}(${days[day]}) ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    // clock.innerText = `${year}-${month}-${today}(${days[day]}) ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+    
+    clock_1.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`    
+    clock_2.innerText = `${days[day]}요일`
+    clock_3.innerText = `${year}.${month}.${today}`;
+    
     
     if (YY){
         YY.innerText = `${year}년 `;
