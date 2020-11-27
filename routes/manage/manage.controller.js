ebb4ff21c5b12103abbc2df2c23fc45835b57b89
tomato8160 <tpgus2303@naver.com>
2020-11-28 01:28:33 +0900

    rebuild routing2

--- routes/manage/manage.controller.js
+++ routes/manage/manage.controller.js
@@ -1,25 +1,44 @@
 const util = require('../../middleware/util');
 
-exports.manageFigures = (res,req) =>{
+exports.manageFigures = (req,res) =>{
     console.log('called manageFigures')
+    
+    res.render('../views/manage.html');
+
 }
 
-exports.newnoticeFigures = (res,req) =>{
+exports.newnoticeFigures = (req,res) =>{
     console.log('called newnoticeFigures')
 
 }
 
-exports.noticeCreate = (res,req) =>{
+exports.noticeCreate = (req,res) =>{
     console.log('called noticeCreate')
 
+    var body = req.body;
+    var _title = body.title;
+    var _contents = body.contents;
+    
+    models.Notice.create({
+        title: _title,
+        contents : _contents
+    })
+    .then(result=>{
+        console.log(result);
+        res.writeHead(302, {Location : '/board'});
+        res.end('success')
+    })
+    .catch(err=>{
+        console.error(err);
+    })
 }
 
-exports.noticeUpdate = (res,req) =>{
+exports.noticeUpdate = (req,res) =>{
     console.log('called noticeUpdate')
 
 }
 
-exports.postFigures = (res,req) =>{
+exports.postFigures = (req,res) =>{
     console.log('called postFigures')
 
 }
