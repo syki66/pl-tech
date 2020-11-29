00d541067fc853df5e354663f5d5f3ebde193ce4
saltcoffee <ybell1028@gmail.com>
2020-11-29 13:42:13 +0900

    new parsing method

--- models/figures.js
+++ models/figures.js
@@ -1,13 +1,17 @@
 exports.firstFigures = (parsed) => {
-    let figures = new Object();
+    let figures = [];
 
-    figures.outsideTemp = parsed[0];
-    figures.heatProd = parsed[1];
-    figures.amount = parsed[2];
-    figures.selfSupp = parsed[3];
-    figures.linkedSupp = parsed[4];
-    figures.linkedSeq = parsed[5];
-    figures.heatSales = parsed[6];
+    let first = new Object();
+
+    first.outsideTemp = parsed[0];
+    first.heatProd = parsed[1];
+    first.amount = parsed[2];
+    first.selfSupp = parsed[3];
+    first.linkedSupp = parsed[4];
+    first.linkedSeq = parsed[5];
+    first.heatSales = parsed[6];
+
+    figures.push(first);
     
 
     return JSON.parse(JSON.stringify(figures));
