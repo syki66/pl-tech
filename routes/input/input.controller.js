89bc98b98dfdcf1fb8593d6298ddf1e777c449e8
saltcoffee <ybell1028@gmail.com>
2020-11-29 13:42:13 +0900

    new parsing method

--- routes/input/input.controller.js
+++ routes/input/input.controller.js
@@ -14,13 +14,54 @@ function parsingFigures(path, callback){
         else {
             console.log('파일 읽기 성공.')
             let parsing = data.split('\r\n'); // 행으로 나누어줌
+
+            // parsing.splice(0, 1);
+            // parsing.splice(7, 2);
+            // parsing.splice(9, 1);
+            // parsing.splice(20, 1);
+            // parsing.splice(23, 1);
+            // parsing.splice(27, 1);
+            // parsing.splice(28, 1);
+            // parsing.splice(35, 1);
+            // parsing.splice(36, 1);
+
+            console.log(parsing);
             let regex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;
-            for (let i = 0; i < parsing.length; i++) {
-                parsing[i] = parsing[i].replace(regex, ""); // 한글 제거
-                parsing[i] = parsing[i].replace(" ", "") // 공백 제거
-                parsing[i] = parsing[i].split(", ");
+
+            let i = 0;
+            let j = 0;
+
+            for (i = 0; i < parsing.length; ++i) {
+                if (parsing[i][0] === '*') {
+                    console.log(parsing[i]) // *1 열생산량 및 판매량
+                    j = i + 1;
+                    while (true) {
+                        if (j >= parsing.length) break;
+                        if (parsing[j] === '') {
+                            parsing.splice(j, 1);
+                            i = j - 1;
+                            break;
+                        }
+                        console.log(parsing[j]);
+                        parsing[j] = parsing[j].replace(regex, ""); // 한글 제거
+                        parsing[j] = parsing[j].replace(" ", "") // 공백 제거
+                        parsing[j] = parsing[j].split(", ");
+                        j++;
+                    }
+                }
             }
+
+
+            // for (let i = 0; i < parsing.length; i++) {
+            //     parsing[i] = parsing[i].replace(regex, ""); // 한글 제거
+            //     parsing[i] = parsing[i].replace(" ", "") // 공백 제거
+            //     if(parsing[i] === '') console.log(i + '까지\n');
+            //     parsing[i] = parsing[i].split(", ");
+            // }
+            console.log(parsing);
+
             let result = figures.firstFigures(parsing);
+        
             callback(null, result);
         }
     });
