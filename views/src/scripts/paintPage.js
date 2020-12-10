5a0b9721fc590ce9dea3b8b480d7356ec52b0035
syki66 <syeonki66@gmail.com>
2020-12-11 02:08:07 +0900

    edit page2,3 & add page4

--- views/src/scripts/paintPage.js
+++ views/src/scripts/paintPage.js
@@ -3,24 +3,26 @@ const rowCount = 12; // 페이지 중에서 최대 row 개수 이상 적으면 �
 const title = document.querySelector('.title');
 const category = document.querySelectorAll('.category');
 
-let rowArray = [];
-for (i=0; i<rowCount; i++){
-    rowArray.push(document.querySelectorAll(`.row__${i}`));
-}
-
-function paintPage(json, num){
+function paintPage(num){
+    const json = JSON.parse(localStorage.getItem('json'));
+    let rowArray = [];
+    for (i = 0; i < rowCount; i++) {
+        rowArray.push(document.querySelectorAll(`.row__${i}`));
+    }
     title.innerText = json.data[num].itemname;
     category.forEach((e,i)=>{
         e.innerText = json.data[num].category[i];
     })
     rowArray.forEach((row, rowIndex) => {
         row.forEach((e, i) => {
-            console.log(e)
             e.innerText = json.data[num][`row${rowIndex}`][i];
             })
     })
 }
 
-const json = JSON.parse(localStorage.getItem('json'));
+paintPage(pageNum-1);
 
-paintPage(json, pageNum-1);
+setInterval(()=>{
+    paintPage(pageNum-1);
+}, 60000)
+
