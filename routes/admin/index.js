f386ec9aef1875b4b66613de6b35b41457442a5b
saltcoffee <ybell1028@gmail.com>
2020-12-15 12:54:43 +0900

    201214

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,28 +1,33 @@
-const util = require('../../middleware/util');
-const express = require('express');
-const controller = require('./admin.controller');
+const util = require("../../middleware/util");
+const express = require("express");
+const controller = require("./admin.controller");
 const router = express.Router();
 
-
 // GET - /admin 관리자 페이지
-router.get('/', controller.admin);
+router.get("/", controller.admin);
 
 // GET - /admin/post 공지 생성 페이지
-router.get('/post', controller.createPost);
+router.get("/post", controller.createPost);
 
 // POST - /admin/post/cprocess 공지 생성 처리 프로세스
-router.post('/post/cprocess', controller.createProcess);
+router.post("/post/cprocess", controller.createProcess);
 
 // GET - /admin/post/manage 공지 관리 페이지
-router.get('/post/manage', controller.managePost);
+router.get("/post/manage", controller.managePost);
 
 // GET - /admin/post/:postnum/update 공지 수정 페이지
-router.get('/post/:postnum/update', controller.updatePost);
+router.get("/post/:postnum/update", controller.updatePost);
 
 // PATCH - /admin/post/:postnum/uprocess 공지 수정 처리 프로세스
-router.patch('/post/:postnum/uprocess', controller.updateProcess);
+router.patch("/post/:postnum/uprocess", controller.updateProcess);
 
 // DELETE - /admin/post/:postnum/dprocess 공지 삭제 처리 프로세스
-router.delete('/post/:postnum/dprocess', controller.deleteProcess);
+router.delete("/post/:postnum/dprocess", controller.deleteProcess);
+
+// GET - /welcome 환영 페이지 렌더링
+router.get("/welcome", controller.welcome);
+
+// POST - /welcome 환영 페이지 입력
+router.post("/welcome", controller.inputWelcome);
 
 module.exports = router;
