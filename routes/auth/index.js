9c30402ed67b249b3ce2943c48837a470facb95d
saltcoffee <ybell1028@gmail.com>
2020-12-15 12:54:43 +0900

    201214

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('./auth.controller');
-
+const controller = require("./auth.controller");
 
 // GET - /auth/login 로그인 페이지
-router.get('/login', controller.login);
+router.get("/login", controller.login);
 
 // POST - /auth/linprocess 로그인 처리 프로세스
-router.post('/linprocess', controller.loginProcess); 
+router.post("/linprocess", controller.loginProcess);
 
 // DELET - /auth/loutprocess 로그아웃 처리 프로세스
-router.delete('/loutprocess', controller.logoutProcess);
+router.post("/loutprocess", controller.logoutProcess);
 
 // GET - /auth/register 관리자 등록 페이지
-router.get('/register', controller.register);
+router.get("/register", controller.register);
 
 // POST - /auth/regprocess 관리자 등록 처리 프로세스
-router.post('/regprocess', controller.registerProcess);
+router.post("/regprocess", controller.registerProcess);
 
 module.exports = router;
