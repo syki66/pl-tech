e177ee21bceb7b03a1d19931ff0ce85ef070a5f1
saltcoffee <ybell1028@gmail.com>
2020-12-15 20:16:28 +0900

    welcome update

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
-const bodyParser = require("body-parser");
+const setting = require("../routes/session");
 
 const board = require("./board");
 const admin = require("./admin");
 const auth = require("./auth");
 const home = require("./home");
+const alert = require("./alert");
 
 // 세션 생성
-router.use('/',session({
-    secret: 'pltech key',
-    resave: false,
-    saveUninitialized: true,
-    store:new MySQLStore({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'rkawk35088',
-        database: 'pltech'
-    })
-  }));
+router.use("/", session(setting.sess));
 
 router.use("/board", board);
 router.use("/admin", admin);
 router.use("/auth", auth);
 router.use("/home", home);
+router.use("/alert", alert);
 
 module.exports = router;
--- views/src/scripts/paintPage.js
+++ views/src/scripts/paintPage.js
@@ -1,28 +1,31 @@
 const rowCount = 12; // 페이지 중에서 최대 row 개수 이상 적으면 됨
 
-const title = document.querySelector('.title');
-const category = document.querySelectorAll('.category');
+const title = document.querySelector(".title");
+const category = document.querySelectorAll(".category");
+const visitor = document.querySelectorAll(".visitor");
+const sentence = document.querySelectorAll(".sentence");
 
-function paintPage(num){
-    const json = JSON.parse(localStorage.getItem('json'));
-    let rowArray = [];
-    for (i = 0; i < rowCount; i++) {
-        rowArray.push(document.querySelectorAll(`.row__${i}`));
-    }
-    title.innerText = json.data[num].itemname;
-    category.forEach((e,i)=>{
-        e.innerText = json.data[num].category[i%3];
-    })
-    rowArray.forEach((row, rowIndex) => {
-        row.forEach((e, i) => {
-            e.innerText = json.data[num][`row${rowIndex}`][i];
-            })
-    })
+function paintPage(num) {
+  const json = JSON.parse(localStorage.getItem("json"));
+  let rowArray = [];
+  for (i = 0; i < rowCount; i++) {
+    rowArray.push(document.querySelectorAll(`.row__${i}`));
+  }
+  title.innerText = json.data[num].itemname;
+  category.forEach((e, i) => {
+    e.innerText = json.data[num].category[i % 3];
+  });
+  rowArray.forEach((row, rowIndex) => {
+    row.forEach((e, i) => {
+      e.innerText = json.data[num][`row${rowIndex}`][i];
+    });
+  });
+  visitor.innerText = json.data[num].visitor;
+  sentence.innerText = json.data[num].sentence;
 }
 
-paintPage(pageNum-1);
+paintPage(pageNum - 1);
 
-setInterval(()=>{
-    paintPage(pageNum-1);
-}, 60000)
-
+setInterval(() => {
+  paintPage(pageNum - 1);
+}, 60000);
