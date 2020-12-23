4add1cb2bba5972aea9b6f90481f8657e1b6bf15
syki66 <66syki@gmail.com>
2020-12-23 22:02:29 +0900

    add vertical-carousel for news

--- views/src/scripts/paintPage.js
+++ views/src/scripts/paintPage.js
@@ -9,31 +9,42 @@ function paintPage(num) {
   for (i = 0; i < rowCount; i++) {
     rowArray.push(document.querySelectorAll(`.row__${i}`));
   }
-  if (title){
-    title.innerText = json.data[0][num].itemname;
-  }
-  if (category){
-    category.forEach((e, i) => {
-      e.innerText =
-        num == 4 ? json.data[0][num].category[i % 3] : json.data[0][num].category[i];
+  if (num == "home"){
+    rowArray.forEach((row, rowIndex) => {
+      row.forEach((e) => {
+          e.innerText = json.data[0][6][`row${rowIndex}`][1];
+      });
     });
-  }
-  rowArray.forEach((row, rowIndex) => {
-    row.forEach((e, i) => {
-      if (num == 8 && json.data[0][num][`row${rowIndex}`][i]){
-        workerImg = `<img style="object-fit: cover; width:100%;" src="/worker/${json.data[num][`row${rowIndex}`][i]}">`;
-        if (e.innerHTML == workerImg){
+  } else{
+    num -= 1;
+
+    if (title){
+      title.innerText = json.data[0][num].itemname;
+    }
+    if (category){
+      category.forEach((e, i) => {
+        e.innerText =
+          num == 4 ? json.data[0][num].category[i % 3] : json.data[0][num].category[i];
+      });
+    }
+    rowArray.forEach((row, rowIndex) => {
+      row.forEach((e, i) => {
+        if (num == 8 && json.data[0][num][`row${rowIndex}`][i]){
+          workerImg = `<img class="image" src="/worker/${json.data[0][num][`row${rowIndex}`][i]}">`;
+          if (e.innerHTML == workerImg){
+            //pass
+          } else{
+            e.innerHTML = workerImg;
+          }
         } else{
-          e.innerHTML = workerImg;
+          e.innerText = json.data[0][num][`row${rowIndex}`][i];
         }
-      } else{
-        e.innerText = json.data[0][num][`row${rowIndex}`][i];
-      }
+      });
     });
-  });
+  }
 }
 
-paintPage(pageNum - 1);
+paintPage(pageNum);
 setInterval(() => {
-  paintPage(pageNum - 1);
+  paintPage(pageNum);
 }, 1000);
