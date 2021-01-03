71725f66708542e207b4a95206a7bb6eb2373966
saltcoffee <ybell1028@gmail.com>
2021-01-04 01:15:26 +0900

    added https(port:433) server and implementing validator 3

--- routes/board/index.js
+++ routes/board/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const controller = require("./board.controller");
+const validator = require('../../middleware/validator');
 const router = express.Router();
 
 // GET /board - 글 목록 출력
+// input validation OK
 router.get("/", controller.noticeList);
 
 // GET /board/:noticeNum - 해당 페이지 글 내용 출력
-router.get("/:noticeNum", controller.noticeContents);
+// input validation OK
+router.get("/:noticeNum", 
+validator.noticeNum,
+validator.pageNum,
+validator.result,
+controller.noticeContents);
 
 // GET /board/more/:pageNum - 공지사항 더보기
-router.get("/more/:pageNum", controller.noticeMore);
+// input validation OK
+router.get("/more/:pageNum",
+validator.pageNum,
+validator.result,
+controller.noticeMore);
 
 module.exports = router;
