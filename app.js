a5df508e45f462f9c6c4f83d18afb82544cbb51e
saltcoffee <ybell1028@gmail.com>
2021-01-23 21:42:48 +0900

    404 not found page added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const http = require("http");
 const https = require("https");
-const fs = require('fs');
+const fs = require("fs");
 const options = {
-	key: fs.readFileSync('./keys/private.pem'),
-	cert: fs.readFileSync('./keys/public.pem')
+    key: fs.readFileSync("./keys/private.pem"),
+    cert: fs.readFileSync("./keys/public.pem"),
 };
 const path = require("path");
 const axios = require("axios");
@@ -22,20 +22,20 @@ const routes = require("./routes");
 const models = require("./models");
 
 models.sequelize
-  .sync()
-  .then(function () {
-    console.log("DB에 연결되었습니다.");
-  })
-  .catch(function (err) {
-    console.error(err);
-    console.log("DB connection error. Please make sure DB is running");
-    process.exit();
-  });
+    .sync()
+    .then(function () {
+        console.log("DB에 연결되었습니다.");
+    })
+    .catch(function (err) {
+        console.error(err);
+        console.log("DB connection error. Please make sure DB is running");
+        process.exit();
+    });
 
 //http environment
 httpApp.set("port", process.env.PORT || 80);
 httpApp.get("*", function (req, res, next) {
-  res.redirect("https://" + req.headers.host + req.path);
+    res.redirect("https://" + req.headers.host + req.path);
 });
 
 //https environment
@@ -59,15 +59,8 @@ axios.defaults.baseURL = ip;
 app.use("/", routes);
 
 app.use(function (req, res, next) {
-  res.status(404);
-  res.json(
-    util.successFalse(
-      {
-        name: "404 Not Found",
-      },
-      "404 Not Found"
-    )
-  );
+    res.status(404);
+    res.render("./404.html");
 });
 
 // app.use (function (req, res, next) {
@@ -80,7 +73,7 @@ app.use(function (req, res, next) {
 //   }
 // });
 
-// app.get('*', function(req, res) {  
+// app.get('*', function(req, res) {
 //   res.redirect('https://' + req.headers.host + req.url);
 //   console.log(req.url);
 //   // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
@@ -92,21 +85,20 @@ app.use(function (req, res, next) {
 //   if (protocol == 'https'){
 //       next();
 //   } else {
-//       let from = `${protocol}://${req.hostname}${req.url}`; 
+//       let from = `${protocol}://${req.hostname}${req.url}`;
 //       let to = `https://${req.hostname}${req.url}`;
-//       // log and redirect 
+//       // log and redirect
 //       console.log(`[${req.method}]: ${from} -> ${to}`);
 //       res.redirect(to);
 //   }
 // })
 
 http.createServer(httpApp).listen(httpApp.get("port"), ip, function () {
-  console.log("ip : " + ip + " | port : " + httpApp.get("port"));
-  console.log("HTTP server is running");
+    console.log("ip : " + ip + " | port : " + httpApp.get("port"));
+    console.log("HTTP server is running");
 });
 
 https.createServer(options, app).listen(app.get("port"), ip, function () {
-  console.log("ip : " + ip + " | port : " + app.get("port"));
-  console.log("HTTPS server is running");
+    console.log("ip : " + ip + " | port : " + app.get("port"));
+    console.log("HTTPS server is running");
 });
-
