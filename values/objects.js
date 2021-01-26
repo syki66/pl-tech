1908b8dddd386035fe40dec0b22cee52bc72cd81
saltcoffee <ybell1028@gmail.com>
2021-01-26 15:52:19 +0900

    Automated DCS values to Json

--- values/objects.js
+++ values/objects.js
@@ -1,7 +1,11 @@
 const slide = require("../values/slide");
 const produce = require("../values/produce");
 
-exports.parsedObj = null;
+exports.parsedObj1 = [];
+exports.parsedObj2 = [];
+exports.parsedObj3 = [];
+exports.parsedObj4 = [];
+exports.parsedObj5 = [];
 exports.noticeObj = [null, null, null, null, null];
 exports.welcomeObj = [null, null];
 exports.safetyObj = [null, null, null, null, null, null];
@@ -11,83 +15,79 @@ exports.lotationObj = [null];
 exports.newsObj = [null];
 
 exports.updateObjects = () => {
-  produce.values = slide.valuesToJson(
-    this.parsedObj,
-    this.welcomeObj,
-    this.noticeObj,
-    this.safetyObj,
-    this.workerObj,
-    this.slideObj,
-    this.lotationObj,
-    this.newsObj
-  );
+    produce.values = slide.valuesToJson(
+        this.parsedObj1,
+        this.parsedObj2,
+        this.parsedObj3,
+        this.parsedObj4,
+        this.parsedObj5,
+        this.welcomeObj,
+        this.noticeObj,
+        this.safetyObj,
+        this.workerObj,
+        this.slideObj,
+        this.lotationObj,
+        this.newsObj
+    );
 };
 
 exports.calcSafety = (zhVal, startDate, targetDate) => {
-  let now = new Date();
+    let now = new Date();
 
-  let yyNow = now.getFullYear(); // 년도
-  let mmNow = now.getMonth() + 1; // 월
-  let ddNow = now.getDate(); // 날짜
+    let yyNow = now.getFullYear(); // 년도
+    let mmNow = now.getMonth() + 1; // 월
+    let ddNow = now.getDate(); // 날짜
 
-  if (mmNow < 10) {
-    mmNow = "0" + mmNow;
-  }
+    if (mmNow < 10) {
+        mmNow = "0" + mmNow;
+    }
 
-  if (ddNow < 10) {
-    ddNow = "0" + ddNow;
-  }
+    if (ddNow < 10) {
+        ddNow = "0" + ddNow;
+    }
 
-  let ymdNow = yyNow + "년 " + mmNow + "월 " + ddNow + "일";
+    let ymdNow = yyNow + "년 " + mmNow + "월 " + ddNow + "일";
 
-  if (startDate === null || targetDate === null) {
-    this.safetyObj = ["-", ymdNow, "-", "-", "설정 필요", "설정 필요"];
-  } else {
-    let yyStart = startDate.substring(0, 4);
-    let yyTarget = targetDate.substring(0, 4);
-    let mmStart;
-    let mmTarget;
-    let ddStart;
-    let ddTarget;
+    if (startDate === null || targetDate === null) {
+        this.safetyObj = ["-", ymdNow, "-", "-", "설정 필요", "설정 필요"];
+    } else {
+        let yyStart = startDate.substring(0, 4);
+        let yyTarget = targetDate.substring(0, 4);
+        let mmStart;
+        let mmTarget;
+        let ddStart;
+        let ddTarget;
 
-    startDate[6] === "0"
-      ? (mmStart = startDate.substring(7, 8))
-      : (mmStart = startDate.substring(6, 8));
+        startDate[6] === "0" ? (mmStart = startDate.substring(7, 8)) : (mmStart = startDate.substring(6, 8));
 
-    targetDate[6] === "0"
-      ? (mmTarget = targetDate.substring(7, 8))
-      : (mmTarget = targetDate.substring(6, 8));
+        targetDate[6] === "0" ? (mmTarget = targetDate.substring(7, 8)) : (mmTarget = targetDate.substring(6, 8));
 
-    startDate[10] === "0"
-      ? (ddStart = startDate.substring(11, 12))
-      : (ddStart = startDate.substring(10, 12));
+        startDate[10] === "0" ? (ddStart = startDate.substring(11, 12)) : (ddStart = startDate.substring(10, 12));
 
-    targetDate[10] === "0"
-      ? (ddTarget = targetDate.substring(11, 12))
-      : (ddTarget = targetDate.substring(10, 12));
+        targetDate[10] === "0" ? (ddTarget = targetDate.substring(11, 12)) : (ddTarget = targetDate.substring(10, 12));
 
-    let start = new Date(yyStart, mmStart, ddStart);
-    let target = new Date(yyTarget, mmTarget, ddTarget);
-    let present = new Date(yyNow, mmNow, ddNow);
+        let start = new Date(yyStart, mmStart, ddStart);
+        let target = new Date(yyTarget, mmTarget, ddTarget);
+        let present = new Date(yyNow, mmNow, ddNow);
 
-    let stGap = start.getTime() - target.getTime(); // 현재 날짜에서 D-day의 차이를 구한다.
-    let stResult = Math.floor(stGap / (1000 * 60 * 60 * 24)) * -1; // gap을 일(밀리초 * 초 * 분 * 시간)로 나눈다. 이 때 -1 을 곱해야 날짜차이가 맞게 나온다.
+        let stGap = start.getTime() - target.getTime(); // 현재 날짜에서 D-day의 차이를 구한다.
+        let stResult = Math.floor(stGap / (1000 * 60 * 60 * 24)) * -1; // gap을 일(밀리초 * 초 * 분 * 시간)로 나눈다. 이 때 -1 을 곱해야 날짜차이가 맞게 나온다.
 
-    let snGap = start.getTime() - present.getTime();
-    let snResult = Math.floor(snGap / (1000 * 60 * 60 * 24)) * -1;
+        let snGap = start.getTime() - present.getTime();
+        let snResult = Math.floor(snGap / (1000 * 60 * 60 * 24)) * -1;
 
-    this.safetyObj = [snResult, ymdNow, zhVal, stResult, startDate, targetDate];
-  }
-  this.updateObjects();
+        this.safetyObj = [snResult, ymdNow, zhVal, stResult, startDate, targetDate];
+    }
+    this.updateObjects();
 };
 
 // 디폴트 값 설정
 
 setTimeout(() => {
-  this.welcomeObj = ["관리자님", "환영문구를\n설정해주세요."];
-  this.calcSafety(this.safetyObj[2], this.safetyObj[4], this.safetyObj[5]);
+    this.welcomeObj = ["관리자님", "환영문구를\n설정해주세요."];
+    this.calcSafety(this.safetyObj[2], this.safetyObj[4], this.safetyObj[5]);
 }, 3000);
 
 setInterval(() => {
-  this.calcSafety(this.safetyObj[2], this.safetyObj[4], this.safetyObj[5]);
+    this.calcSafety(this.safetyObj[2], this.safetyObj[4], this.safetyObj[5]);
 }, 1000 * 60 * 60 * 6);
--- values/slide.js
+++ values/slide.js
@@ -1,142 +1,91 @@
 const produce = require("../values/produce");
 
 exports.valuesToJson = (
-  parsedObj,
-  welcomeObj,
-  noticeObj,
-  safetyObj,
-  workerObj,
-  slideObj,
-  lotationObj,
-  newsObj
+    parsedObj1,
+    parsedObj2,
+    parsedObj3,
+    parsedObj4,
+    parsedObj5,
+    welcomeObj,
+    noticeObj,
+    safetyObj,
+    workerObj,
+    slideObj,
+    lotationObj,
+    newsObj
 ) => {
-  if (!produce.parsingErr) {
-    let values = [];
+    if (!produce.parsingErr) {
+        let values = [];
 
-    let pageValues = [];
+        let pageValues = [];
 
-    let item_1 = new Object();
+        for (let i = 1; i <= 5; ++i) {
+            const item = new Object();
+            const parsedObj = eval("parsedObj" + i);
+            let oIdx = 0;
+            let rIdx = 0;
+            item.itemname = parsedObj[oIdx++];
+            if (i != 2) item.category = parsedObj[oIdx++];
+            for (let j = oIdx; j < parsedObj.length; ++j) {
+                eval("item.row" + rIdx++ + "= parsedObj[" + j + "]");
+            }
+            pageValues.push(item);
+        }
 
-    item_1.itemname = parsedObj[0];
-    item_1.category = parsedObj[1];
-    item_1.row0 = parsedObj[2];
-    item_1.row1 = parsedObj[3];
-    item_1.row2 = parsedObj[4];
-    item_1.row3 = parsedObj[5];
-    item_1.row4 = parsedObj[6];
-    item_1.row5 = parsedObj[7];
-    item_1.row6 = parsedObj[8];
+        let item_6 = new Object();
 
-    pageValues.push(item_1);
+        item_6.itemname = ["환영 문구"];
+        item_6.row0 = [welcomeObj[0]];
+        item_6.row1 = [welcomeObj[1]];
 
-    let item_2 = new Object();
+        pageValues.push(item_6);
 
-    item_2.itemname = parsedObj[9]; //*2
-    item_2.row0 = parsedObj[10];
-    item_2.row1 = parsedObj[11];
-    item_2.row2 = parsedObj[12];
-    item_2.row3 = parsedObj[13];
-    item_2.row4 = parsedObj[14];
-    item_2.row5 = parsedObj[15];
-    item_2.row6 = parsedObj[16];
-    item_2.row7 = parsedObj[17];
-    item_2.row8 = parsedObj[18];
-    item_2.row9 = parsedObj[19];
-    item_2.row10 = parsedObj[20];
-    item_2.row11 = parsedObj[21]; // 판교 연계
+        if (noticeObj !== null) {
+            let item_7 = new Object();
+            item_7.itemname = ["게시판 주요 업무"];
+            item_7.row0 = noticeObj[0];
+            item_7.row1 = noticeObj[1];
+            item_7.row2 = noticeObj[2];
+            item_7.row3 = noticeObj[3];
+            item_7.row4 = noticeObj[4];
+            pageValues.push(item_7);
+        }
 
-    pageValues.push(item_2);
+        let item_8 = new Object();
+        item_8.itemname = ["무재해 기록판"];
+        item_8.row0 = [safetyObj[0]];
+        item_8.row1 = [safetyObj[1]];
+        item_8.row2 = [safetyObj[2]];
+        item_8.row3 = [safetyObj[3]];
+        item_8.row4 = [safetyObj[4]];
+        item_8.row5 = [safetyObj[5]];
 
-    let item_3 = new Object();
+        pageValues.push(item_8);
 
-    item_3.itemname = parsedObj[22];
-    item_3.category = parsedObj[23];
-    item_3.row0 = parsedObj[24];
-    item_3.row1 = parsedObj[25];
-    item_3.row2 = parsedObj[26];
+        if (workerObj !== null) {
+            let item_9 = new Object();
+            item_9.itemname = ["근무자 현황"];
+            item_9.row0 = workerObj[0];
+            item_9.row1 = workerObj[1];
+            item_9.row2 = workerObj[2];
+            item_9.row3 = workerObj[3];
+            pageValues.push(item_9);
+        }
 
-    pageValues.push(item_3);
+        values.push(pageValues);
 
-    let item_4 = new Object();
+        // values 2번째 요소
+        let slideValues = [];
 
-    item_4.itemname = parsedObj[27];
-    item_4.category = parsedObj[28];
-    item_4.row0 = parsedObj[29];
-    item_4.row1 = parsedObj[30];
-    item_4.row2 = parsedObj[31];
-    item_4.row3 = parsedObj[32];
-    item_4.row4 = parsedObj[33];
-    item_4.row5 = parsedObj[34];
+        let slide = new Object();
+        slide.itemname = ["슬라이드 및 뉴스탭 설정"];
+        slide.row0 = slideObj;
+        slide.row1 = lotationObj;
+        slide.row2 = newsObj;
+        slideValues.push(slide);
 
-    pageValues.push(item_4);
+        values.push(slideValues);
 
-    let item_5 = new Object();
-
-    item_5.itemname = parsedObj[35];
-    item_5.category = parsedObj[36];
-    item_5.row0 = parsedObj[37];
-    item_5.row1 = parsedObj[38];
-    item_5.row2 = parsedObj[39];
-    item_5.row3 = parsedObj[40];
-    item_5.row4 = parsedObj[41];
-    item_5.row5 = parsedObj[42];
-
-    pageValues.push(item_5);
-
-    let item_6 = new Object();
-
-    item_6.itemname = ["환영 문구"];
-    item_6.row0 = [welcomeObj[0]];
-    item_6.row1 = [welcomeObj[1]];
-
-    pageValues.push(item_6);
-
-    if (noticeObj !== null) {
-      let item_7 = new Object();
-      item_7.itemname = ["게시판 주요 업무"];
-      item_7.row0 = noticeObj[0];
-      item_7.row1 = noticeObj[1];
-      item_7.row2 = noticeObj[2];
-      item_7.row3 = noticeObj[3];
-      item_7.row4 = noticeObj[4];
-      pageValues.push(item_7);
+        return JSON.parse(JSON.stringify(values));
     }
-
-    let item_8 = new Object();
-    item_8.itemname = ["무재해 기록판"];
-    item_8.row0 = [safetyObj[0]];
-    item_8.row1 = [safetyObj[1]];
-    item_8.row2 = [safetyObj[2]];
-    item_8.row3 = [safetyObj[3]];
-    item_8.row4 = [safetyObj[4]];
-    item_8.row5 = [safetyObj[5]];
-
-    pageValues.push(item_8);
-
-    if (workerObj !== null) {
-      let item_9 = new Object();
-      item_9.itemname = ["근무자 현황"];
-      item_9.row0 = workerObj[0];
-      item_9.row1 = workerObj[1];
-      item_9.row2 = workerObj[2];
-      item_9.row3 = workerObj[3];
-      pageValues.push(item_9);
-    }
-
-    values.push(pageValues);
-
-    // values 2번째 요소
-    let slideValues = [];
-
-    let slide = new Object();
-    slide.itemname = ["슬라이드 및 뉴스탭 설정"];
-    slide.row0 = slideObj;
-    slide.row1 = lotationObj;
-    slide.row2 = newsObj;
-    slideValues.push(slide);
-
-    values.push(slideValues);
-
-    return JSON.parse(JSON.stringify(values));
-  }
 };
